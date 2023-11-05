39b8b27380a35623e519166b0b0ad2502022663a
Hanandiya Putra <137038046+hannntempest@users.noreply.github.com>
2023-11-05 23:12:32 +0700

    Add files via upload

---
+++ src/kamus.js
@@ -0,0 +1,6 @@
+export const dictionary = [
+    "PHP", "JavaScript", "C++", "HTML", "CSS", "Java", "C", "C#", "Go",
+    "React", "Node", "Ruby", "Python", "SQL", "Integer", "String", "Array", "Object",
+    "Boolean", "Date", "DateTime", "Scan", "Input", "Output", "Print", "Append", "Prepend",
+    "Add", "Subtract", "Multiply", "Divide", "Modulus", "Percent"
+];
